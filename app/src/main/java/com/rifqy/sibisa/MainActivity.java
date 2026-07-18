8a0bb32bb8cb609ad619c21400ee4018be7a20db
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2026-07-18 18:23:18 +0700

    feat: integrate Unity AR module with Android app

--- app/src/main/java/com/rifqy/sibisa/MainActivity.java
+++ app/src/main/java/com/rifqy/sibisa/MainActivity.java
@@ -7,18 +7,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button btnAR = findViewById(R.id.btnAR);
+
+        btnAR.setOnClickListener(v -> {
+
+            Intent intent = new Intent(
+                    MainActivity.this,
+                    com.unity3d.player.UnityPlayerGameActivity.class
+            );
+
+            startActivity(intent);
+
         });
     }
 }
