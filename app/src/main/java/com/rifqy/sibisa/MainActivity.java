abbdebb4b31b15e98e8d64a58ae6a59cbc7dba8a
Muhammad Shahril Ramdan <bloodwings40@gmail.com>
2026-07-19 10:59:33 +0700

    Update halaman utama dengan tombol AR Camera

--- app/src/main/java/com/rifqy/sibisa/MainActivity.java
+++ app/src/main/java/com/rifqy/sibisa/MainActivity.java
@@ -1,7 +1,9 @@
 package com.rifqy.sibisa;
 
 import android.os.Bundle;
-
+import android.content.Intent;
+import android.view.View;
+import android.widget.ImageButton;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -10,15 +12,37 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private View btnKuis, btnFaktaBuah, btnThirdActivity;
+    private ImageButton btnArCamera;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Inisialisasi Tombol dari XML
+        btnKuis = findViewById(R.id.btnKuis);
+        btnFaktaBuah = findViewById(R.id.btnFaktaBuah);
+        btnThirdActivity = findViewById(R.id.btnThirdActivity);
+        btnArCamera = findViewById(R.id.btnArCamera);
+
+        // Aksi ketika tombol Kuis diklik
+        btnKuis.setOnClickListener(v -> {
+            // Intent intent = new Intent(MainActivity.this, KuisActivity.class);
+            // startActivity(intent);
+        });
+
+        // Aksi ketika tombol Fakta Buah diklik
+        btnFaktaBuah.setOnClickListener(v -> {
+            // Intent intent = new Intent(MainActivity.this, FaktaBuahActivity.class);
+            // startActivity(intent);
+        });
+
+        // Aksi ketika tombol AR diklik
+        btnArCamera.setOnClickListener(v -> {
+            // Di sini nanti kita arahkan ke ArCameraActivity
+            // Intent intent = new Intent(MainActivity.this, ArCameraActivity.class);
+            // startActivity(intent);
         });
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,6 +22,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "SIBISA"
+rootProject.name = "Tubes"
 include(":app")
- 
