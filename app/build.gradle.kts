ed26e248f32e664d82b84bfd49c69ad2aeccea11
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2026-07-25 07:19:07 +0700

    build: configure Android project for Unity integration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.rifqy.sibisa"
-        minSdk = 26
+        minSdk = 30
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -30,12 +30,13 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
 dependencies {
+    implementation(project(":unityLibrary"))
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,12 +15,19 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+
     repositories {
         google()
         mavenCentral()
+
+        flatDir {
+            dirs(file("unityLibrary/libs"))
+        }
     }
 }
 
 rootProject.name = "Tubes"
 include(":app")
+include(":unityLibrary")
+include(":unityLibrary:xrmanifest.androidlib")
